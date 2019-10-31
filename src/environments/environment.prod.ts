d16210f8b6d7e2ef093f9dda59033a3866c07841
billal1993 <m.billalyaqoob@gmail.com>
2019-10-31 15:44:36 +0500

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBnAWaPDnkt7ykmkFdPHR0ZoipmeXXTo7U",
+    authDomain: "angular-project-oshop.firebaseapp.com",
+    databaseURL: "https://angular-project-oshop.firebaseio.com",
+    projectId: "angular-project-oshop",
+    storageBucket: "angular-project-oshop.appspot.com",
+    messagingSenderId: "789408245194",
+    appId: "1:789408245194:web:a22dca307483d718e15b33",
+    measurementId: "G-XFG0YPH0CT"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBnAWaPDnkt7ykmkFdPHR0ZoipmeXXTo7U",
+    authDomain: "angular-project-oshop.firebaseapp.com",
+    databaseURL: "https://angular-project-oshop.firebaseio.com",
+    projectId: "angular-project-oshop",
+    storageBucket: "angular-project-oshop.appspot.com",
+    messagingSenderId: "789408245194",
+    appId: "1:789408245194:web:a22dca307483d718e15b33",
+    measurementId: "G-XFG0YPH0CT"
+  }
 };
 
 /*
