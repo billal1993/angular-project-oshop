6f028916157ac06ce7a3214765c7a1cd09606153
billal1993 <m.billalyaqoob@gmail.com>
2019-11-04 16:38:40 +0500

    product filter feature implemented

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: {title: string}[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll();
-    // console.log(this.products$);
+    //getting all products in products array
+    this.subscription = this.productService.getAll()
+    .subscribe(products => this.filteredProducts = this.products =products);
+   }
+   //to filter the objects
+   filter(query: string){
+    this.filteredProducts = (query) ?
+    //if there is something in query then
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+    //otherwise do this
+      this.products;
+   }
+
+   ngOnDestroy(){
+    this.subscription.unsubscribe();
    }
 
   ngOnInit() {
