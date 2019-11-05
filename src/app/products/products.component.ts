10bd683dd22e5f26bf75e8b77492b7a187594374
billal1993 <m.billalyaqoob@gmail.com>
2019-11-05 20:45:38 +0500

    applied filter on products in home page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -9,14 +12,36 @@ import { CategoryService } from '../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[]= [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    //getting all products
-    this.products$ = productService.getAll(); 
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService, 
+    categoryService: CategoryService) {
+    //getting all products in products
+    productService
+    .getAll()
+    //with this switchMap we can switch from one abservable to another
+      //here we get all the products 
+    .pipe(switchMap(products => {
+      this.products = products;
+      //here switching to another observable
+      return route.queryParamMap;
+    }))
+      //getting route parameters
+     .subscribe(params => {
+        this.category = params.get('category');
+        //now here filter applied
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
     //getting all categories
     this.categories$ = categoryService.getAll();
+
    }
 
 }
